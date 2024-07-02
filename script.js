0353b47ed6c350a1aae5fec06bed17a6369a53b7
Ethan Walker <101420077+ethan-walker@users.noreply.github.com>
2024-07-02 14:37:39 +0000

    pull events from data.json

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function createCalendar(data) {
 		let nearestMultiple = Math.ceil((daysInMonth + firstDay) / 7) * 7;
 		
 
-		let month_elem = document.createElement("section");
+		var month_elem = document.createElement("section");
 		month_elem.classList.add("month");
 
 		month_elem.dataset.month = months[(startMonthNum + i) % 12]
@@ -76,5 +76,45 @@ function createCalendar(data) {
 	createEvents(data);
 }
 function createEvents(data) {
-	// waa
+	let startMonth = new Date(data.setup.startMonth);
+	let startMonthNum = startMonth.getMonth();
+	
+	for (var event of data.events) {
+		let start = new Date(event.start);
+		let end = new Date(event.end);
+
+		var loop = start;
+		while (loop <= end) {
+			var current_month = loop.getMonth() - startMonthNum + 1;
+			var current_day = loop.getDate();
+
+			var parent = document.querySelector(`.month:nth-of-type(${current_month}) > div:nth-child(${current_day} of :not(.blank))`)
+			let elem = document.createElement("div");
+			elem.classList.add("event");
+			for (var elem_class of event.class) {
+				elem.classList.add(elem_class)
+			}
+			
+			if (loop === start) {
+				elem.classList.add("start");
+			}
+			else if (loop === end) {
+				elem.classList.add("end");
+			}
+			
+			parent.appendChild(elem);
+
+			loop = new Date(loop.setDate(loop.getDate() + 1));
+		}
+	}
+	const style = document.createElement("style");
+	document.head.appendChild(style);
+	const styleSheet = style.sheet;
+
+	let rules = ""
+	for (var item of data.classes) {
+		console.log(item)
+		var rule = "." + item.name + " {" + "background-color:" + "var(--color-" + item.color + "); grid-row:" + item.row + ";}\n"
+		styleSheet.insertRule(rule, 0);
+	}
 }
